a8b46b2ae49d2cda1917c8942358ef0623745a6d
covard <ovardcj@gmail.com>
2013-07-21 13:36:33 -0600

    add base rakefile,gemfile, and database.yml files, add specs to start from, spec/code for init_variables

--- lib/ruby_app_generator.rb
+++ lib/ruby_app_generator.rb
@@ -2,15 +2,31 @@ require "ruby_app_generator/version"
 
 module RubyAppGenerator
   class Generator
-    def output(app_name)
-      puts "File#dirname: #{File.dirname(__FILE__)}"
-      puts "Dir#pwd: #{Dir.pwd}"
-      generate_base_files app_name
+
+    def generate app_name
+      init_variables
+    end
+
+    def init_variables
+      @wd = Dir.pwd
+      @gem_dir = File.dirname(__FILE__)
+
+      [@wd, @gem_dir]
     end
 
     def generate_base_files(app_name)
       puts
       puts "Generating ruby app #{app_name} in #{Dir.pwd}/#{app_name}"
     end
+
+    def init_rspec
+      
+    end
+
+    def init_guard
+      
+    end
+
   end
+
 end
---
+++ spec/lib/ruby_app_generator_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe RubyAppGenerator::Generator do
+  before :each do
+    @generator = RubyAppGenerator::Generator.new
+  end
+
+  describe '#generate' do
+    it { should respond_to :generate }
+  end
+
+  describe '#generate_base_files' do
+    it { should respond_to :generate_base_files }
+    it 'creates a rakefile'
+
+    it 'creates a gemfile'
+
+    it 'creates a readmefile'
+  end
+
+  describe '#init_rspec' do
+    it { should respond_to :init_rspec }
+    it 'initializes respec'
+  end
+
+  describe '#init_guard' do
+    it { should respond_to :init_guard }
+    it 'initializes guard'
+  end
+
+  describe '#init_variables' do
+    it { should respond_to :init_variables }
+    it 'initializes and returns 2 instance variables' do
+      vars = []
+      vars = @generator.init_variables
+      expect(vars.length).to eq 2
+    end
+  end
+  
+end
